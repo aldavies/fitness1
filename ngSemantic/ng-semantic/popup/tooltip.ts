089b1f9b585695a5d002858e9ced2dda7379bc90
aldavies <aldavies@umich.edu>
2016-11-27 18:41:06 -0500

    first commit

---
+++ ngSemantic/ng-semantic/popup/tooltip.ts
@@ -0,0 +1,24 @@
+import { Directive, ViewContainerRef, Input, OnInit } from "@angular/core";
+
+declare var jQuery: any;
+
+/**
+ * Implementation of Semantic UI popup
+ *
+ * @link http://semantic-ui.com/modules/popup.html
+ */
+@Directive({
+    selector: "[smDirTooltip]"
+})
+export class SMTooltipDirective implements OnInit {
+
+    @Input() smDirTooltip: string;
+    @Input() smDirPosition: string;
+
+    constructor(public element: ViewContainerRef) {}
+
+    ngOnInit(): void {
+        this.element.element.nativeElement.setAttribute("data-position", this.smDirPosition || "top center");
+        this.element.element.nativeElement.setAttribute("data-tooltip", this.smDirTooltip);
+    }
+}
