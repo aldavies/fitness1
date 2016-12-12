44c9c4ad1421f098c46fe22cac5aa4dd2b7eab20
Aleksei Miasin <alexei.miasin@gmail.com>
2016-12-11 20:32:00 -0500

    Created Starter Project with Angular2_CLI and Semantic UI integrated

---
+++ Fitness_Angular2-CLI/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <sm-segment class="raised">
+        Hello Fitness App on Angular 2 and Semantic UI
+    </sm-segment>
+    `
+})
+export class AppComponent {}
